8d81feca3e4d54560ff9155f79e7f6e606ae3aa9
marinakravchuk <105381706+marinakravchuk@users.noreply.github.com>
2022-10-20 18:56:23 +0300

    test 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
+
+// console.log(galleryItems);
+const getItems = document.querySelector('.gallery');
+
+function onMarkUpImages (items) {
+    return items.map(item => `<li class="gallery__item"><a class="gallery__link" href="${item.original}"/><img class="gallery__image" src="${item.preview}" alt="${item.description}"/></li>`).join('');
+};
+
+const callFunc = onMarkUpImages(galleryItems);
+getItems.innerHTML = callFunc;
+
+//============================================================
+
+getItems.addEventListener('click', onMarkUpGallery);
+
+function onMarkUpGallery (event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    };
+};
+const lightbox =  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
