e057be067be550bea4643e4eb27f2189d8dd62ad
marinakravchuk <105381706+marinakravchuk@users.noreply.github.com>
2022-10-20 19:32:43 +0300

    test 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,26 @@
 import Player from '@vimeo/player';
+import throttle from'lodash.throttle';
+const KEY = 'videoplayer-current-time';
+const iframe = document.querySelector("iframe");
+const time = localStorage.getItem(KEY)
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
+const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
+player.on('timeupdate', throttle(function(currentTime ) {
+
+    localStorage.setItem( KEY, currentTime.seconds  )
+ },1000));
+
+player.setCurrentTime(time).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from'lodash.throttle';
 
+const getFormValue = document.querySelector('.feedback-form')
+const getButton = document.querySelector('button');
+const getInputValue = document.querySelector('input');
+const getTextareaValue = document.querySelector('textarea');
+
+const FEEDBACKKEYS = 'feedback-form-state';
+
+getFormValue.addEventListener('input', throttle(onInputValue, 500));
+getButton.addEventListener('click', onSendFormInput);
+
+//=======================================================================
+function onInputValue () {
+    const arr = {email: getInputValue.value, message: getTextareaValue.value};
+    const feedback = JSON.stringify(arr);
+    localStorage.setItem(FEEDBACKKEYS, feedback);
+};
+//=======================================================================
+function onSavedInputValue () {
+    const savedInput = JSON.parse(localStorage.getItem(FEEDBACKKEYS));
+    if (savedInput) {
+        getInputValue.value = savedInput.email;
+        getTextareaValue.value = savedInput.message;
+    };
+};
+onSavedInputValue()
+//=======================================================================
+function onSendFormInput (event) {
+    const arr = {email: getInputValue.value, message: getTextareaValue.value};
+    event.preventDefault ();
+    getFormValue.reset();
+    localStorage.removeItem(FEEDBACKKEYS);
+    console.log(arr)
+};
